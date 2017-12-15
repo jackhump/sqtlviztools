bfb34d0d527ac573966c2b6eec1af725ad93d617
Jack Humphrey <jack.humphrey@ucl.ac.uk>
2017-12-15 12:29:05 +0000

    first commit

---
+++ R/make_sQTL_box_plot.R
@@ -0,0 +1,122 @@
+#  sel <- 1
+#  cluster_to_plot = row.names(resultsToPlot)[sel]
+#  # get lowest p value junction in cluster to start
+#  all_junctions = sigJunctions
+#  junction_to_plot= all_junctions[all_junctions$clu==cluster_to_plot,]
+#  junction_to_plot = junction_to_plot[ which( junction_to_plot$bpval == min(junction_to_plot$bpval) ), ]$pid
+# #                    main_title = "test"
+#                    vcf=vcf
+#                     vcf_meta=vcf_meta
+#                     exons_table = exons_table
+#                     counts = clusters
+#                     introns = annotatedClusters
+#                     cluster_ids = annotatedClusters$clusterID
+#                     snp_pos = resultsToPlot[sel,]$SNP_pos
+#                     snp = resultsToPlot[sel,]$SNP
+# #
+# #
+#
+
+
+#' Make genotype x junction count box plots
+#'
+#' @import ggplot2
+#' @import ggbeeswarm
+#' @import dplyr
+#' @export
+make_sQTL_box_plot <- function(
+  cluster_to_plot,
+  all_junctions = NA,
+  junction_to_plot = NA,
+  main_title = NA,
+  exons_table = NULL,
+  vcf = NULL,
+  vcf_meta = NULL,
+  cluster_ids = NULL,
+  counts = NULL,
+  introns = NULL,
+  snp_pos=NA,
+  junctionTable = NA,
+  snp = snp ){
+
+  #print("HELLO JACK")
+  #print(junction_to_plot)
+
+  #print(snp)
+
+  stopifnot( snp %in% vcf_meta$SNP )
+
+  # sometimes chr is missing
+
+  if( !grepl("^chr", junction_to_plot)){
+    junction_to_plot <- paste0("chr", junction_to_plot)
+  }
+
+  # subset VCF and get genotype groups
+  vcfIndex <- which( vcf_meta$SNP == snp)
+  VCF <- vcf[vcfIndex,10:ncol(vcf)]
+  #table(t(VCF) )
+  #for testing!
+
+  VCF_meta <- vcf_meta[vcfIndex,]
+  #message("I AM INSIDE YOUR CODE")
+  meta <- as.data.frame(t(VCF))
+
+  print(VCF_meta)
+  #print(meta)
+  #message("YO")
+  meta$group=as.factor(meta[,1])
+  #message("DAWG")
+  group_names <- c(0,1,2)
+  names(group_names) <- c(
+    paste0( VCF_meta$REF, "/", VCF_meta$REF),
+    paste0( VCF_meta$REF, "/", VCF_meta$ALT),
+    paste0( VCF_meta$ALT, "/", VCF_meta$ALT)
+  )
+
+  #print(group_names)
+  #message("BOYYYYYY")
+  y <- t(counts[ cluster_ids==cluster_to_plot, ])
+  #message("HELLO BOY")
+  # for each sample divide each junction count by the total for that sample
+  normalisedCounts <- as.data.frame(sweep(y, 1, rowSums(y), "/"))
+
+  genotypes <- as.data.frame(t(VCF))
+  names(genotypes)[1] <- "geno"
+  #message("WHEEWWWW LADDDY")
+  normalisedCounts$genotypeCode <- genotypes$geno[ match( row.names(normalisedCounts), row.names(genotypes))]
+  normalisedCounts <- normalisedCounts[ complete.cases(normalisedCounts),]
+
+  normalisedCounts$genotype <- names(group_names)[ match(normalisedCounts$genotypeCode, group_names)]
+
+  print(head(normalisedCounts))
+  #message("GREAT TO MEET YOU")
+
+  toPlot <- dplyr::select( normalisedCounts,
+                    junction = junction_to_plot,
+                    geno =  "genotype")
+  #message("I AM BEAT")
+  toPlot$geno <- factor(toPlot$geno, levels = (names(group_names))) # this was reversed
+
+  #message("HELLO AGAIN LADDY")
+
+  # get junction information for title
+  junc <- dplyr::mutate(junctionTable,
+                 j = paste0( gsub("-",":", coord),":", clu)
+                 ) %>%
+    dplyr::filter( j == junction_to_plot )
+
+  values <- paste( signif(as.numeric(junc$Beta),3)," q =", signif(as.numeric(junc$q),3) )
+  #message("STILL HERE BOY")
+  plot <- ggplot( data = toPlot, aes(x = geno, y = junction, group = geno ) ) +
+    geom_boxplot(outlier.colour = NA, fill = "orange") +
+    geom_quasirandom(size = 0.8) + #coord_flip() +
+    theme_classic() +
+    theme(axis.title.y = element_text( angle = 90 ) ) +
+    ylab("contribution to cluster") +
+    xlab("") +
+    labs(title = junc$coord,
+         subtitle = bquote(beta==.(values)) )
+
+  return(plot)
+}
---
+++ R/sQTL_functions.R
@@ -0,0 +1,191 @@
+# functions used in preparing sQTLs for browsing
+
+get_snp_meta <- function(snps){
+  snp_meta <-  as.data.frame(stringr::str_split_fixed(snps, "\\.", 3), stringsAsFactors = FALSE)
+  colnames(snp_meta) <- c("snp_ID", "snp_chr", "snp_pos")
+  # a few snps don't have any IDs - just the coordinates
+  noID <- snp_meta[ grepl("\\.", snp_meta$snp_pos),]
+  noID <- dplyr::select(noID, snp_pos, snp_ID, snp_chr)
+  names(noID) <- c("snp_ID", "snp_chr","snp_pos")
+  # put back in
+  snp_meta[ grepl("\\.", snp_meta$snp_pos),] <- noID
+
+  snp_meta$snp_pos <- as.numeric(snp_meta$snp_pos)
+  return(snp_meta)
+}
+
+
+get_vcf_meta <- function(vcf){
+  # take a VCF and split out the meta data
+  # to use for querying
+  vcf_meta <-  as.data.frame(stringr::str_split_fixed(vcf$ID, "\\.", 3), stringsAsFactors = FALSE)
+  vcf_meta$SNP_pos <- paste(vcf_meta$V2, vcf_meta$V3, sep = ":")
+  vcf_meta <- data.frame( SNP = vcf_meta$V1, SNP_pos = vcf_meta$SNP_pos, REF = vcf$REF, ALT = vcf$ALT, stringsAsFactors = FALSE)
+  return(vcf_meta)
+}
+
+intersect_introns <- function(introns){
+  all.introns <- introns
+  all.introns$start <- as.numeric(all.introns$start)
+  all.introns$end <- as.numeric(all.introns$end)
+
+
+  # for each splice site write out a bed file
+  all.junctions <- dplyr::select(all.introns, chr, start, end, clusterID = clu)
+
+  all.fiveprime <- data.frame( chr = all.introns$chr,
+                               start = all.introns$start,
+                               end = all.introns$start + 1,
+                               clusterID = all.introns$clu)
+  all.threeprime <- data.frame( chr = all.introns$chr,
+                                start = all.introns$end,
+                                end = all.introns$end + 1,
+                                clusterID = all.introns$clu)
+  all.file <- "all_junctions.bed"
+  all.fiveprime.file <- "all_fiveprime.bed"
+  all.threeprime.file <- "all_threeprime.bed"
+
+  write.table( all.junctions, all.file, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t" )
+  write.table( all.threeprime, all.threeprime.file, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t" )
+  write.table( all.fiveprime, all.fiveprime.file, col.names = FALSE, row.names = FALSE, quote = FALSE, sep = "\t" )
+
+  print( "BedTools intersect junctions with list of known splice sites")
+
+  # first match junctions
+  all.introns.cmd <- paste0("bedtools intersect -a ", all.file, " -b ", all_introns, " -wa -wb -loj -f 1" )
+  all.introns_intersect <- data.table::fread(all.introns.cmd)
+
+  # intersect with bedtools to find the annotations of each splice site
+  threeprime.cmd <- paste0( "bedtools intersect -a ", all.threeprime.file, " -b ",threeprime_file, " -wa -wb -loj -f 1" )
+  threeprime_intersect <- data.table::fread(threeprime.cmd)
+
+  fiveprime.cmd <- paste0( "bedtools intersect -a ", all.fiveprime.file, " -b ", fiveprime_file, " -wa -wb -loj -f 1" )
+  fiveprime_intersect <- data.table::fread(fiveprime.cmd)
+
+  # remove temporary files
+  rm.cmd <- paste("rm ", all.file, all.fiveprime.file, all.threeprime.file)
+  system(rm.cmd)
+
+  return( list(threeprime_intersect, fiveprime_intersect,all.introns_intersect))
+}
+
+annotate_single_cluster <- function(introns, clu, cluIndex){
+  #print(clu)
+  # for each intron in the cluster, check for coverage of both
+  # output a vector of string descriptions
+  cluster <- introns[ introns$clu == clu , ]
+  cluster$start <- as.integer(cluster$start)
+  cluster$end <- as.integer(cluster$end)
+  # subset intersects by clusterID (V4)
+  fprimeClu <- fiveprime_intersect[ V4 == clu,]
+  tprimeClu <- threeprime_intersect[ V4 == clu,]
+  bothSSClu <- all.introns_intersect[ V4 == clu,]
+  # for each intron in the cluster:
+  #   create vector of overlapping splice sites, indexed by the row of the intersect
+  # five prime splice sites
+  # some clusters only have 1 junction so it's simple
+  if( nrow(cluster) == 1 ){
+    fprime <- list(fprimeClu)
+    tprime <- list(tprimeClu)
+    bothSS <- list(bothSSClu)
+    cluster_genes <- unique( c(tprime[[1]]$V8, fprime[[1]]$V8, bothSS[[1]]$V8 ) )
+    cluster_ensemblIDs <- unique( c(tprime[[1]]$V9, fprime[[1]]$V9, bothSS[[1]]$V9 ) )
+  }else{
+    fprime <- apply( cluster, MAR = 1, FUN = function(x) {
+      chr <- which( names(cluster) == "chr" )
+      start <- which( names(cluster) == "start" )
+      fprimeClu[
+        V1 == x[chr] &
+          V2 == as.numeric( x[start] ),]
+    } )
+
+    # three prime splice sites
+    tprime <- apply( cluster, MAR = 1, FUN = function(x) {
+      chr <- which( names(cluster) == "chr" )
+      end <- which( names(cluster) == "end" )
+      tprimeClu[
+        V1 == x[chr] &
+          V2 == as.numeric( x[end] ),]
+    } )
+
+    # both splice sites
+    bothSS <-  apply( cluster, MAR = 1, FUN = function(x) {
+      chr <- which( names(cluster) == "chr" )
+      start <- which(names(cluster) == "start")
+      end <- which( names(cluster) == "end" )
+
+      bothSSClu[
+        V6 == as.numeric( x[start] ) &
+          V7 == as.numeric( x[end] ) ,]
+    } )
+    # find gene and ensemblID by the most represented gene among all the splice sites
+    cluster_genes <- names(sort(table(do.call( what = rbind, c(tprime, fprime, bothSS)  )$V8), decreasing = TRUE ))
+    cluster_ensemblIDs <- names(sort(table(do.call( what = rbind, c(tprime, fprime, bothSS) )$V9), decreasing = TRUE ))
+  }
+
+  cluster_gene <- cluster_genes[ cluster_genes != "." ][1]
+  # if no cluster gene found then leave as "."
+  if( length(cluster_gene) == 0){
+    cluster_gene == "."
+  }
+
+  cluster_ensemblID <- cluster_ensemblIDs[ cluster_ensemblIDs != "." ][1]
+  if( length( cluster_ensemblID ) == 0 ){
+    cluster_ensemblID == "."
+  }
+
+  verdict <- c()
+  coord <- c()
+  gene <- c()
+  ensemblID <- c()
+  transcripts <- list()
+
+  for( intron in 1:nrow(cluster) ){
+    coord[intron] <- paste0(cluster[intron,]$chr,":", cluster[intron,]$start,"-", cluster[intron,]$end )
+
+    gene[intron] <- cluster_gene
+    ensemblID[intron] <- cluster_ensemblID
+
+    # for each intron create vector of all transcripts that contain both splice sites
+    #transcripts[[intron]] <- unique( intersect( tprime[[intron]]$V10, fprime[[intron]]$V10 ) )
+
+    verdict[intron] <- "error"
+    if( # if neither are annotated
+      all( tprime[[intron]]$V5 == ".") & all( fprime[[intron]]$V5 == "." )
+    ){ verdict[intron] <- "cryptic_unanchored"
+    }
+    if( # if only one is annotated
+      all( tprime[[intron]]$V5 == ".") & all( fprime[[intron]]$V5 != "." )
+    ){ verdict[intron] <- "cryptic_threeprime"
+    }
+    if(
+      all( tprime[[intron]]$V5 != ".") & all( fprime[[intron]]$V5 == "." )
+    ){ verdict[intron] <- "cryptic_fiveprime"
+    }
+    if( # if both splice sites are annotated
+      all( tprime[[intron]]$V5 != "." ) & all( fprime[[intron]]$V5 != "." )
+    ){
+      # test if the splice sites are paired in a known intron
+      if( all(bothSS[[intron]]$V5 != ".") ){
+        verdict[intron] <- "annotated"
+      }else{ # both are annotated but never in the same junction
+        verdict[intron] <- "novel annotated pair"
+      }
+    }
+
+  }
+  if( cluIndex %% 500 == 0 ){
+    print( paste("processed", cluIndex, "clusters" ))
+  }
+  #print(clu)
+
+  return(
+    data.frame(
+      clusterID = clu,
+      coord = coord,
+      gene = gene,
+      ensemblID = ensemblID,
+      verdict = verdict,
+      stringsAsFactors = FALSE)
+  )
+}
